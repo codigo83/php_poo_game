07954b5d1964d038aa9068de3c86e53c8960fef5
Carlos_83 <carlos.codigo83@gmail.com>
2017-09-03 19:45:51 +0200

    Batalla

--- herencia.php
+++ herencia.php
@@ -1,8 +1,15 @@
 <?php
-	
+
+function show($message){
+	echo "<p>{$message}</p>";
+}
+
+
 abstract class Unit{
 
+
 	protected $alive;
+	protected $hp= 100;
 	protected $name;
 
 	public function __construct($name){
@@ -10,8 +17,59 @@ abstract class Unit{
 		$this->alive= true;
 	}
 
+
+	/*SETTER Y GETTERS*/
+	public function getName(){
+		return $this->name;
+	}
+
+
+	public function getAlive(){
+		return $this->alive;
+	}
+
+	public function getHp(){
+		return $this->hp;
+	}
+
+	public function setHp($hp){
+		$this->hp = $hp;
+	}
+
+
 	public function move($direction){
-		echo "<p>{$this->name} se mueve hacia $direction</p>";
+		show( "{$this->name} se mueve hacia $direction" );
+	}
+
+	//Otras funciones
+
+	public function takeDamage($damage){
+
+
+		$this->hp= $this->hp - $damage;
+
+		if($this->hp <= 0){
+			$this->die();
+		}else{
+
+			show("<strong>{$this->name}</strong> pierde {$damage} puntos de vida");
+		}
+
+	
+	}
+
+	public function die(){
+
+		if($this->getAlive()){
+
+			$this->alive= false;
+			show("<mark style='background-color:IndianRed'><strong>{$this->getName()}</strong> ha muerto</mark>");	
+
+		}else{
+			show("Le estas pegando a un cadaver");
+		}
+
+		
 	}
 
 	public abstract function attack(Unit $opponent);
@@ -25,27 +83,116 @@ abstract class Unit{
 
 class Soldier extends Unit{
 
+	protected $damage= 38;
+
 	public function attack(Unit $opponent){
-		echo "<p>{$this->name} corta en dos a {$opponent->name}</p>";
+
+		if( $this->getAlive() && $opponent->getAlive()){
+
+			show( "<mark style='background-color:yellow' >({$this->getHp()}) {$this->name}</mark> golpea con su espada a  {$opponent->name}" );
+			$opponent->takeDamage( $this->damage );
+		}
+		
+
 	}
+
+
+	public function takeDamage($damage){
+		
+		return parent::takeDamage( $damage/2 );
+	
+	}
+
 }
 
 
 class Archer extends Unit{
 
+	protected $damage= 30;
+	
+
 	public function attack(Unit $opponent){
-		echo "<p>{$this->name} dispara una flecha a {$opponent->name}</p>";
+
+		if($this->getAlive() && $opponent->getAlive() ){
+
+			show( "<mark style='background-color:lightblue'>({$this->getHp()}) {$this->name}</mark> dispara una flecha a {$opponent->name}" );
+			$opponent->takeDamage( $this->damage );
+
+		}
+	}
+
+	public function takeDamage($damage){
+
+		if( rand(0,1) ){
+			
+			show("{$this->getName()} esquivó el ataque");
+		}else{
+			parent::takeDamage( $damage );
+		}
+	
 	}
 }
 
 
 
-$soldado= new Soldier('Warrino');
+class Battle{
+	private $a;
+	private $b;
+	
+	public function __construct(Unit $a, Unit $b){
+		$this->a= $a;
+		$this->b= $b;
+	}
+
+	public function start(){
+		show("<p><strong>Comienza la batalla entre {$this->a->getName()} y {$this->b->getName()}</strong></p>");
+		
+		while($this->status()){
+
+			$this->a->attack($this->b);
+			$this->b->attack($this->a);
+		}
+		
+		if($this->a->getAlive())
+			$winner= $this->a->getName();
+		else
+			$winner= $this->b->getName();
+
+		show("<mark style='background-color: lightgreen'>El vencedor del duelo és <strong>$winner</strong></mark>");
+
+	}
+
+	public function status(){
+		if($this->a->getAlive() && $this->b->getAlive() )
+			return true;
+		else
+			return false;
+	}
+}
+
+
+
+$soldado= new Soldier('Áragon');
 $arquero= new Archer('Legolas');
 
-$soldado->move('la izquierda');
+$batalla= new Battle( $soldado, $arquero);
 
-$soldado->attack($arquero);
+$batalla->start();
+
+
+
+
+		
+
+
+
+
+
+
+//$arquero->die();
+//$arquero->die();
+
+
 
 
 

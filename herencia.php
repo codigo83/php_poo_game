9dda3106ece48d397290c24662be3f8e6bd9aa7b
Carlos_83 <carlos.codigo83@gmail.com>
2017-09-03 16:26:00 +0200

    Inicio

---
+++ herencia.php
@@ -0,0 +1,24 @@
+<?php
+	
+class Unit{
+
+}
+
+class Soldier{
+
+}
+
+class Archer{
+
+}
+
+echo 'Hello 2';
+echo '<br/>';
+echo 'Esto es otra cosa';
+echo '<br/>';
+echo 'tercera linea 2';
+echo '<br/>';
+echo 'nueva linea';
+echo '<br/>';
+echo 'nueva lineas';
+?>
